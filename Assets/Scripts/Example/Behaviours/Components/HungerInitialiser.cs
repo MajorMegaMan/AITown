8ec9913415a90e9e1fd39dd7e03c0030270ad7e8
Lucas Petersen <ima_lucas@hotmail.com>
2021-05-14 15:33:36 +1000

    WIP Testing Scriptable objects
    They may not be a very good Idea at all. Maybe will scrap them

---
+++ Assets/Scripts/Example/Behaviours/Components/HungerInitialiser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GOAP;
+
+public class HungerInitialiser : BehaviourInitialiser
+{
+    public HungerInitialiser()
+    {
+        foreach (var act in ActionList.humanFoodActions)
+        {
+            actionList.Add(act);
+        }
+
+        requiredWorldStates = new GOAPWorldState();
+
+        var worldState = requiredWorldStates;
+        worldState.CreateElement(WorldValues.holdItemType, WorldValues.HoldItemType.nothing);
+        worldState.CreateElement(WorldValues.holdItemObject, null);
+
+        worldState.CreateElement(WorldValues.hunger, 100.0f);
+        worldState.CreateElement(WorldValues.hasProcessedHunger, false);
+
+        updater = new HungerUpdate();
+    }
+}
--- Assets/Scripts/GOAP/TestScriptable.cs
+++ Assets/Scripts/GOAP/TestScriptable.cs
@@ -6,33 +6,59 @@ using UnityEditor;
 [CreateAssetMenu(fileName = "TestyBoy", menuName = "Tests/FirstGuy")]
 public class TestScriptable : ScriptableObject
 {
-    
     public MonoScript script;
+
+    public List<MonoScript> actions;
+    public MonoScript behaviourUpdater;
 
     private void OnValidate()
     {
-        Thing();
+        ValidateUpdater();
+        ValidateActions();
     }
 
-    void Thing()
+    void ValidateActions()
     {
-        if(script == null)
+        if(actions.Count == 0)
         {
             return;
         }
 
-        var type = script.GetClass();
+        for(int i = 0; i < actions.Count; i++)
+        {
+            if (actions[i] == null)
+            {
+                return;
+            }
 
-        if(type.IsSubclassOf(typeof(AIAgentAction)))
-        {
-            // AIAction
-            Debug.Log("all good");
-        }
-        else
-        {
-            Debug.LogWarning("Script is not AIAgentActionType");
-            script = default;
+            var type = actions[i].GetClass();
+
+            if (!type.IsSubclassOf(typeof(AIAgentAction)))
+            {
+                Debug.LogWarning("Script is not " + typeof(AIAgentAction));
+                actions[i] = default;
+            }
         }
     }
 
+    void ValidateUpdater()
+    {
+        ValidateMonoScript(ref behaviourUpdater, typeof(BehaviourUpdater), "Script is not BehaviourUpdater");
+    }
+
+    void ValidateMonoScript(ref MonoScript mono, System.Type classType, string message)
+    {
+        if (mono == null)
+        {
+            return;
+        }
+
+        var type = mono.GetClass();
+
+        if (!type.IsSubclassOf(classType))
+        {
+            Debug.LogWarning(message);
+            mono = default;
+        }
+    }
 }
